bbeec9377d5bb8e6f7217fe97000e966f96e639e
DungDao23092005 <dungdao108@gmail.com>
2025-11-12 21:47:30 +0700

    update

---
+++ source_code/feature_contribution/src/main/java/com/stushare/feature_contribution/ui/upload/UploadViewModel.kt
@@ -0,0 +1,8 @@
+package com.stushare.feature_contribution.ui.upload
+
+import androidx.lifecycle.ViewModel
+
+// ViewModel kế thừa (extends) từ lớp ViewModel của AndroidX
+class UploadViewModel : ViewModel() {
+    // Chúng ta sẽ thêm logic (như hàm "handleUploadClick") vào đây sau
+}
